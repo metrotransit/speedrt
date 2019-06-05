370c6ff1778ece42c8bfc21c6fdafb10b145c14d
Joey Reid <jpreid@icloud.com>
2019-06-05 12:06:58 -0500

    Add checks for suggested packages

--- R/launch_speedapp.R
+++ R/launch_speedapp.R
@@ -11,6 +11,10 @@
 #' @param ... passed to shiny::runApp
 #' @export
 launch_speedapp <- function(sao = NULL, rstudio = Sys.getenv("RSTUDIO") == "1", ...) {
+	# check for suggested packages required for app
+	pkgs <- c('ggplot2', 'leaflet', 'rgdal', 'shiny', 'shinydashboard', 'stringi')
+	pkg_exists <- suppressWarnings(sapply(pkgs, requireNamespace, quietly = TRUE))
+	if (!all(pkg_exists)) stop(gettextf(fmt = 'SpeedApp requires one or more packages to be installed. Please install: %s and try again: install.packages(c(%s))', paste(names(pkg_exists)[!pkg_exists], collapse = ', '), paste(sQuote(names(pkg_exists)[!pkg_exists]), collapse = ', ')), call. = FALSE)
   launch.browser <- if(!rstudio) {
 		TRUE 
     } else {
