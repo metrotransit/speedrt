563e8e646d7fdce7be1b6a6a6204bf94552e1619
Joey Reid <joseph.reid@metrotransit.org>
2019-06-20 07:52:15 -0500

    Move remaining helpers to ui.R
    Re #4

--- inst/SpeedApp/helpers.R
+++
@@ -1,5 +0,0 @@
-libs <- c('speedRT', 'data.table', 'DT', 'd3heatmap', 'ggplot2', 'ggmap', 'leaflet', 'rgeos', 'rgdal', 'shiny', 'shinydashboard', 'sp', 'stringi', 'zoo', 'sf')
-
-suppressMessages(invisible(lapply(libs, library, character.only = TRUE)))
-
-options(shiny.maxRequestSize = 100 * 1024^2)
--- inst/SpeedApp/ui.R
+++ inst/SpeedApp/ui.R
@@ -1,4 +1,9 @@
-source('helpers.R')
+libs <- c('speedRT', 'data.table', 'DT', 'd3heatmap', 'ggplot2', 'ggmap', 'leaflet', 'rgeos', 'rgdal', 'shiny', 'shinydashboard', 'sp', 'stringi', 'zoo', 'sf')
+
+suppressMessages(invisible(lapply(libs, library, character.only = TRUE)))
+
+options(shiny.maxRequestSize = 100 * 1024^2)
+
 
 fluidPage(
   tags$head(
